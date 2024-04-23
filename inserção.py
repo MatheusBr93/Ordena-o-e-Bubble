bfc5b5dae38e058d1494e06c09b84b9f141596f4
MatheusBr93 <146784410+MatheusBr93@users.noreply.github.com>
2024-04-23 20:05:13 -0300

    Update inserção.py

--- inserção.py
+++ inserção.py
@@ -1,15 +1,15 @@
 def insertionSort(alist):
    for index in range(1,len(alist)):
 
-     currentvalue = alist[index]
+     valor = alist[index]
      posição = index
      print(alist)
 
-     while posição>0 and alist[posição-1]>currentvalue:
+     while posição>0 and alist[posição-1]>valor:
          alist[posição]=alist[posição-1]
          posição = posição-1
 
-     alist[posição]=currentvalue
+     alist[posição]=valor
 
 alist = [15, 5, 4, 18, 12, 19, 14, 10, 8, 20]
 insertionSort(alist)
